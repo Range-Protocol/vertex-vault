9328454649c152c74e503ecfad790e769fb45c24
0xbateman <0xbateman@rangeprotocol.com>
2024-03-26 20:04:53 +0800

    update code

--- script/interfaces/IUSDC.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
-
-interface IUSDC {
-    function mint(address, uint256) external;
-    function approve(address, uint256) external;
-    function transfer(address, uint256) external;
-}
